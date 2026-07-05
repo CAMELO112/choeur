44443d62b7b4d3f641bcba9fdd2ae3587a33b7d6
CAMELO112 <camelo.janvier@gmail.com>
2026-07-06 00:32:02 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,26 @@
 import './globals.css'
 import NavBar from './components/NavBar'
+import { PlayerProvider } from './components/PlayerContext'
+import PlayerBar from './components/PlayerBar'
 
 export const metadata = {
-  title: 'Choeur',
-  description: 'Plateforme de streaming musical et communautaire',
+    title: 'Choeur',
+    description: 'Plateforme de streaming musical et communautaire',
 }
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="fr">
-    <body>
-    <header className="topbar">
-    <a href="/" className="logo">Choeur</a>
-  <NavBar />
-    </header>
-  <main>{children}</main>
-    </body>
-    </html>
-  )
+    return (
+          <html lang="fr">
+            <body>
+              <PlayerProvider>
+                <header className="topbar">
+                  <a href="/" className="logo">Choeur</a>
+              <NavBar />
+      </header>
+            <main>{children}</main>
+            <PlayerBar />
+      </PlayerProvider>
+      </body>
+      </html>
+    )
 }
